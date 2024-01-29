36813d0f406279715f863c107cd813377338e09d
Deepak Verma <deepak@webllisto.com>
2024-01-29 15:08:06 +0530

    fix port error in deployment

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
